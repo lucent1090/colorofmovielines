abab18d0d50823d09aa8444eb0b54d8b3f507513
lucent1090 <lucent1090@gmail.com>
2017-02-06 12:00:50 +0800

    initial

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Main extends React.Component{
+	render(){
+		return(
+			<h1>main</h1>
+		);
+	}
+}
+
+ReactDOM.render(<Main />, document.getElementById('main'));
